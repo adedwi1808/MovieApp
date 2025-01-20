5ab8cfad7e827e788575f294b605dbc930443b97
Ade Dwi Prayitno <adedwip1808@gmail.com>
2025-01-20 20:08:16 +0700

    add: mainscreenvc

---
+++ MovieApp/Presentation/MainScreen/MainScreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainScreenViewController.swift
+//  MovieApp
+//
+//  Created by Ade Dwi Prayitno on 20/01/25.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
