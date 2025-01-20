87749241dfc5f1ded5fa459961b5b21e1a0e4dcc
Ade Dwi Prayitno <adedwip1808@gmail.com>
2025-01-20 21:52:51 +0700

    add: MoviesResponseModel

---
+++ MovieApp/Domainless/Model/Response/MoviesResponseModel.swift
@@ -0,0 +1,57 @@
+//
+//  MoviesResponseModel.swift
+//  MovieApp
+//
+//  Created by Ade Dwi Prayitno on 20/01/25.
+//
+
+import Foundation
+
+// MARK: - MoviesResponseModel
+struct MoviesResponseModel: Codable {
+    let page: Int?
+    let results: [MovieResponseModel]?
+    let totalPages, totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - MovieResponseModel
+struct MovieResponseModel: Codable {
+    let adult: Bool?
+    let backdropPath: String?
+    let genreIDS: [Int]?
+    let id: Int?
+    let originalLanguage, originalTitle, overview: String?
+    let popularity: Double?
+    let posterPath, releaseDate, title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
+
+extension MovieResponseModel {
+    func mapToMovie() -> Movie {
+        Movie(id: self.id ?? 0, posterPath: self.posterPath ?? "")
+    }
+}
