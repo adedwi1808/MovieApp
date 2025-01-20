eb4338d2be0b4826f0c064020736d892adfbee71
Ade Dwi Prayitno <adedwip1808@gmail.com>
2025-01-20 20:35:26 +0700

    add: movie model

---
+++ MovieApp/Domainless/Model/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  MovieApp
+//
+//  Created by Ade Dwi Prayitno on 20/01/25.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let id: Int
+    let posterPath: String?
+}
